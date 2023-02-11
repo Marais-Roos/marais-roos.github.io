d0afa4f4440b6439d2fcb9e342e142355a9f5bca
MRoos99 <maraisroos@outlook.com>
2023-02-11 18:04:57 +0200

    Commt responsive nav menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,3 +106,15 @@ function loadCopywriting() {
 //
 // Hamburger Menu
 //
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
